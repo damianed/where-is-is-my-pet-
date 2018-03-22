8f6530dd4743bde6c68969033432c2af6b3a253e
elPulpo495 <pestala495@gmail.com>
2018-03-22 00:56:55 -0700

    Se arreglo error de Imagen de fondo.

--- app/src/main/java/com/example/damian/wheresmypet/MainActivity.java
+++ app/src/main/java/com/example/damian/wheresmypet/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.damian.wheresmypet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtUser, txtPass;
+    Button btnOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtUser=findViewById(R.id.txtUser);
+        txtPass=findViewById(R.id.txtPass);
+        btnOk=findViewById(R.id.btnOk);
     }
 }
