6ca6a06db7f7a87eb918021b2a3870011b95b4fd
elPulpo495 <pestala495@gmail.com>
2018-04-18 13:18:25 -0600

    Agregue pantalla de menu principal y cambie el fondo del login

--- app/src/main/java/com/example/damian/wheresmypet/MainMenu.java
+++ app/src/main/java/com/example/damian/wheresmypet/MainMenu.java
@@ -1,11 +1,13 @@
 package com.example.damian.wheresmypet;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainMenu extends AppCompatActivity {
+public class menu extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
     }
 }
